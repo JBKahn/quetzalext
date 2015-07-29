644ea3221d7a29aea5ff7aae6224e769a0054b5e
Jamison Hyman <jhyman2@umbc.edu>
2015-07-28 23:48:14 -0400

    Onclick function

--- context_menu.js
+++ context_menu.js
@@ -1,3 +1,10 @@
+function genericOnClick(info, tab) {
+  console.log("item " + info.menuItemId + " was clicked");
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+  alert(info.selectionText + ". Great choice, I drink that one a lot");
+}
+
 var parent1 = chrome.contextMenus.create({
   title: "Quetzale",
   contexts: ["selection"]
@@ -7,21 +14,13 @@ var child1 = chrome.contextMenus.create({
   title: "Search on BeerAdvocate",
   parentId: parent1,
   contexts: ["selection"],
-  onclick: function(info, tab){
-    console.log("item " + info.menuItemId + " was clicked");
-    console.log("info: " + JSON.stringify(info));
-    console.log("tab: " + JSON.stringify(tab));
-  }
+  onclick: genericOnClick
 });
 
 var child2 = chrome.contextMenus.create({
   title: "Search on some other beer website",
   parentId: parent1,
   contexts: ["selection"],
-  onclick: function(info, tab){
-    console.log("item " + info.menuItemId + " was clicked");
-    console.log("info: " + JSON.stringify(info));
-    console.log("tab: " + JSON.stringify(tab));
-  }
+  onclick: genericOnClick
 });
 
